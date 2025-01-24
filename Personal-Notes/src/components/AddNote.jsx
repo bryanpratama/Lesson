8edd16c992ba0f89c6d0fc2fa9cc07d5dae65434
bryanpratama <bpratama116@gmail.com>
2025-01-24 08:40:45 +0700

    try to fix max title length

--- Personal-Notes/src/components/AddNote.jsx
+++ Personal-Notes/src/components/AddNote.jsx
@@ -4,6 +4,9 @@ function AddNote({ onAddNote }) {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+  // Maksimal panjang judul
+  const MAX_TITLE_LENGTH = 50;
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && body) {
@@ -18,15 +21,27 @@ function AddNote({ onAddNote }) {
     }
   };
 
+  // Fungsi pembatas judul
+  const handleTitleChange = (e) => {
+    if (e.target.value.length <= MAX_TITLE_LENGTH) {
+      setTitle(e.target.value);
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} className="note-input">
-      <input
-        type="text"
-        placeholder="Masukkan judul..."
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
+      <div className="note-input__title">
+        <input
+          type="text"
+          placeholder="Masukkan judul..."
+          value={title}
+          onChange={handleTitleChange}
+          required
+        />
+        <p className="note-input__title__char-limit">
+          Sisa karakter: {MAX_TITLE_LENGTH - title.length}
+        </p>
+      </div>
       <textarea
         placeholder="Masukkan isi catatan..."
         value={body}
--- Personal-Notes/src/components/NoteItem.jsx
+++ Personal-Notes/src/components/NoteItem.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { showFormattedDate } from "../utils";
 
 function NoteItem({ note, handleDeleteNote }) {
+  const { id, title, body, createdAt } = note;
+
   return (
     <div className="note-item">
-      <div className="note-item__content">
-        <h3 className="note-item__title">{note.title}</h3>
-        <p className="note-item__body">{note.body}</p>
-      </div>
-      <div className="note-item__action">
-        <button
-          className="note-item__delete-button"
-          onClick={() => handleDeleteNote(note.id)}
-        >
-          Hapus
-        </button>
-      </div>
-    </div>
+  <div className="note-item__content">
+    <div className="note-item__icon"></div>
+    <h3 className="note-item__title">{title}</h3>
+    <p className="note-item__date">{showFormattedDate(createdAt)}</p>
+    <p className="note-item__body">{body}</p>
+  </div>
+  <div className="note-item__action">
+    <button className="note-item__delete-button" onClick={() => handleDeleteNote(id)}>Hapus</button>
+  </div>
+</div>
+
   );
 }
 
