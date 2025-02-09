b8304de6d342237aab14bc929c8d157c7f1921e6
bryanpratama <bpratama116@gmail.com>
2025-02-09 15:34:38 +0700

    add validation calculator number

--- Javascript/calculator.js
+++ Javascript/calculator.js
@@ -5,25 +5,34 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Masukkan angka pertama: ', (num1) => {
-    rl.question('Masukkan angka kedua: ', (num2) => {
+function askNumber(question, callback) {
+    rl.question(question, (input) => {
+        if (isNaN(input)) {
+            console.log('Input harus berupa angka! Silakan coba lagi.');
+            askNumber(question, callback);
+        } else {
+            callback(parseFloat(input));
+        }
+    });
+}
+
+askNumber('Masukkan angka pertama: ', (num1) => {
+    askNumber('Masukkan angka kedua: ', (num2) => {
         rl.question('Pilih operasi (tambah, kurang, kali, bagi): ', (operation) => {
-            const a = parseFloat(num1);
-            const b = parseFloat(num2);
             let result;
 
             switch (operation.toLowerCase()) {
                 case 'tambah':
-                    result = a + b;
+                    result = num1 + num2;
                     break;
                 case 'kurang':
-                    result = a - b;
+                    result = num1 - num2;
                     break;
                 case 'kali':
-                    result = a * b;
+                    result = num1 * num2;
                     break;
                 case 'bagi':
-                    result = b !== 0 ? a / b : 'Tidak bisa membagi dengan nol';
+                    result = num2 !== 0 ? num1 / num2 : 'Tidak bisa membagi dengan nol';
                     break;
                 default:
                     result = 'Operasi tidak dikenali';
