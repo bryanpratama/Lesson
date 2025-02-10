e9f983f3caaa6bcd6c24c502da2a9870efe094e9
bryanpratama <bpratama116@gmail.com>
2025-02-10 14:09:26 +0700

    fix error in animal

--- Javascript/animal.js
+++ Javascript/animal.js
@@ -29,38 +29,37 @@
 
 // Tulis kode di bawah ini
 class Animal {
-    constructor(name, age, isMammal) {
+  constructor(name, age, isMammal) {
       this.name = name;
       this.age = age;
       this.isMammal = isMammal;
-    }
   }
-  
-  class Rabbit extends Animal {
-    constructor(name, age) {
+}
+
+class Rabbit extends Animal {
+  constructor(name, age) {
       super(name, age, true);
-    }
-  
-    eat() {
-      return ${this.name} sedang makan!;
-    }
   }
-  
-  class Eagle extends Animal {
-    constructor(name, age) {
+
+  eat() {
+      return `${this.name} sedang makan!`;
+  }
+}
+
+class Eagle extends Animal {
+  constructor(name, age) {
       super(name, age, false);
-    }
-  
-    fly() {
-      return ${this.name} sedang terbang!;
-    }
   }
-  
-  const myRabbit = new Rabbit("Labi", 2);
-  
-  const myEagle = new Eagle("Elo", 4);
-  
-  console.log(myRabbit.eat());
-  console.log(myEagle.fly());
-  console.log(myRabbit);
-  console.log(myEagle);
+
+  fly() {
+      return `${this.name} sedang terbang!`;
+  }
+}
+
+const myRabbit = new Rabbit("Labi", 2);
+const myEagle = new Eagle("Elo", 4);
+
+console.log(myRabbit.eat());
+console.log(myEagle.fly());
+console.log(myRabbit);
+console.log(myEagle);
