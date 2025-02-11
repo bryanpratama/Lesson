96ca28d4ed7608399bdf9d4f2c22baf725d4ec1f
bryanpratama <bpratama116@gmail.com>
2025-02-11 13:03:37 +0700

    fix minor problem in author

--- Javascript/author.js
+++ Javascript/author.js
@@ -24,6 +24,6 @@ const books = [
   // Tulis kode di bawah ini
   const greatAuthors = books
     .filter(book => book.sales > 1000000)
-    .map(book => ${book.author} adalah penulis buku ${book.title} yang sangat hebat!);
+    .map(book => `${book.author} adalah penulis buku ${book.title} yang sangat hebat!`);
   
   console.log(greatAuthors);
