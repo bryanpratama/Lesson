34ed3d9c664bfd314b9f5d80a48c0a363abf5c31
bryanpratama <bpratama116@gmail.com>
2025-03-05 16:18:34 +0700

    add support for numbers up to nine hundred and ninety nine

--- Javascript/number-to-words-converter.js
+++ Javascript/number-to-words-converter.js
@@ -9,12 +9,18 @@ const angkaKeKata = (num) => {
   const satuan = ["", "satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan"];
   const belasan = ["sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"];
   const puluhan = ["", "", "dua puluh", "tiga puluh", "empat puluh", "lima puluh", "enam puluh", "tujuh puluh", "delapan puluh", "sembilan puluh"];
-  
+
   if (num < 10) return satuan[num];
   if (num < 20) return belasan[num - 10];
   if (num < 100) return puluhan[Math.floor(num / 10)] + (num % 10 !== 0 ? " " + satuan[num % 10] : "");
+  if (num < 1000) {
+    let ratusan = Math.floor(num / 100);
+    let sisa = num % 100;
+    let ratusanKata = ratusan === 1 ? "seratus" : satuan[ratusan] + " ratus";
+    return ratusanKata + (sisa !== 0 ? " " + angkaKeKata(sisa) : "");
+  }
 
-  return "Belum mendukung angka besar!";
+  return "Belum mendukung angka lebih dari 999!";
 };
 
 rl.question("Masukkan angka: ", (angka) => {
