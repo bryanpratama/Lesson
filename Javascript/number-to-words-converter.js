4ad1e468ff1007760e7475cc951eb1d00bd18719
bryanpratama <bpratama116@gmail.com>
2025-04-01 20:36:31 +0700

    extend number to words support up to 99 unvigintiliun

--- Javascript/number-to-words-converter.js
+++ Javascript/number-to-words-converter.js
@@ -42,8 +42,8 @@ const konversiTigaDigit = (num) => {
 
 rl.question("Masukkan angka: ", (angka) => {
   let num = BigInt(angka);
-  if (num > 9999999999999999999999999999999999999999999999999999999999999999999n) {
-    console.log("Maaf, angka terlalu besar! Maksimum adalah 9 unvigintiliun.");
+  if (num > 99999999999999999999999999999999999999999999999999999999999999999999n) {
+    console.log("Maaf, angka terlalu besar! Maksimum adalah 99 unvigintiliun.");
   } else {
     console.log(`Hasil: ${angkaKeKata(num)}`);
   }
